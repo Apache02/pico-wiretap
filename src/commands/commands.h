921568591a5479b15b3b1e4a5d5bd787d6c5796a
Aleksey Priymachuk <gxad02@gmail.com>
2026-03-06 22:56:37 +0200

    fix: i2c scan on static pins

--- src/commands/commands.h
+++ src/commands/commands.h
@@ -1,6 +1,8 @@
 #pragma once
 
 
+int command_pinout(int argc, const char *argv[]);
+
 int command_clocks(int, const char *[]);
 
 int command_sensors(int, const char *[]);
@@ -10,4 +12,3 @@ int command_i2c_scan(int, const char *[]);
 int command_chip_id(int, const char *[]);
 
 int command_tasks(int argc, const char *argv[]);
-
--- src/commands/i2c_scan.cpp
+++ src/commands/i2c_scan.cpp
@@ -4,64 +4,34 @@
 #include "hardware/i2c.h"
 #include "hardware/gpio.h"
 
+#define I2C_PIN_SDA     2
+#define I2C_PIN_SCL     3
+#undef I2C_INSTANCE
+#define I2C_INSTANCE    i2c1
+#define DEFAULT_BAUDRATE (100 * 1000)
 
-#define DEFAULT_BAUDRATE        (100 * 1000)
 
 static bool reserved_addr(uint8_t addr) {
     return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
 }
 
-static void usage(const char *command) {
-    printf("\n    Usage: %s <pin1: 0-21> <pin2: 0-21> [baudrate: default %d]\n\n", command, DEFAULT_BAUDRATE);
-}
-
-
 int command_i2c_scan(int argc, const char *argv[]) {
-    auto pin1_r = take_int(argv[1]);
-    auto pin2_r = take_int(argv[2]);
-    auto baudrate = take_int(argv[3]).ok_or(DEFAULT_BAUDRATE);
+    auto baudrate = take_int(argv[1]).ok_or(DEFAULT_BAUDRATE);
 
-    if (pin1_r.is_err() || pin2_r.is_err()) {
-        printf("Error: incorrect pins\n");
-        usage(argv[0]);
-        return 1;
-    }
+    gpio_init(I2C_PIN_SDA);
+    gpio_init(I2C_PIN_SCL);
+    gpio_set_function(I2C_PIN_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_PIN_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_PIN_SDA);
+    gpio_pull_up(I2C_PIN_SCL);
 
-    auto pin1 = (int) pin1_r;
-    auto pin2 = (int) pin2_r;
-
-    if (pin1 > 21 || pin2 > 21 || pin1 < 0 || pin2 < 0) {
-        printf("Error: pins must be in range [0-21]\n");
-        return 1;
-    }
-
-    if (pin1 == pin2) {
-        printf("Error: pins can't be equal\n");
-        return 1;
-    }
-
-    if ((pin1 & 2) != (pin2 & 2)) {
-        printf("Error: pins must have same i2c instance\n");
-        return 1;
-    }
-
-    gpio_init(pin1);
-    gpio_init(pin2);
-    gpio_set_function(pin1, GPIO_FUNC_I2C);
-    gpio_set_function(pin2, GPIO_FUNC_I2C);
-    gpio_pull_up(pin1);
-    gpio_pull_up(pin2);
-
-    auto instance = i2c_get_instance((pin1 >> 1) & 1);
-    i2c_init(instance, baudrate);
+    i2c_init(I2C_INSTANCE, baudrate);
 
     printf("\n");
-    printf("Pins: %d + %d\n", pin1, pin2);
-    printf("Instance: %s\n", instance == i2c0 ? "i2c0" : "i2c1");
+    printf("SDA: GP%d  SCL: GP%d\n", I2C_PIN_SDA, I2C_PIN_SCL);
     printf("Baudrate: %d\n", baudrate);
 
-    printf("\ni2c_write_blocking test\n");
-    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+    printf("\n   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
 
     for (int addr = 0; addr < (1 << 7); ++addr) {
         if (addr % 16 == 0) {
@@ -73,18 +43,17 @@ int command_i2c_scan(int argc, const char *argv[]) {
         if (reserved_addr(addr)) {
             ret = PICO_ERROR_GENERIC;
         } else {
-            ret = i2c_write_blocking(instance, addr, &txdata, 1, false);
+            ret = i2c_write_blocking(I2C_INSTANCE, addr, &txdata, 1, false);
         }
 
         printf(ret < 0 ? "." : "@");
         printf(addr % 16 == 15 ? "\n" : "  ");
     }
 
-    // reset pins
-    gpio_init(pin1);
-    gpio_init(pin2);
-    gpio_disable_pulls(pin1);
-    gpio_disable_pulls(pin2);
+    gpio_init(I2C_PIN_SDA);
+    gpio_init(I2C_PIN_SCL);
+    gpio_disable_pulls(I2C_PIN_SDA);
+    gpio_disable_pulls(I2C_PIN_SCL);
 
     return 0;
 }
